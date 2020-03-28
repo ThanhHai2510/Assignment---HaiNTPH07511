9cea42326b14c71acb5c0d4cbbb5e2bed9dadc8f
ThanhHai2510 <toilathanhhaitep@gmail.com>
2020-03-28 19:44:43 +0700

    5 files modified

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Book } from "../Book";
+import { BookService } from "../book.service";
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.css"]
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  books;
+  constructor(private bookService: BookService) {}
 
   ngOnInit() {
+    this.getBooks();
   }
 
+  getBooks() {
+    this.bookService
+      .getBooks()
+      .subscribe(
+        response => (this.books = response),
+        error => console.log(error)
+      );
+  }
 }
