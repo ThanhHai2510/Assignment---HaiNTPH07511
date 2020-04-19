60e0494f7bc928193851e117e7d3776a1b279556
ThanhHai2510 <toilathanhhaitep@gmail.com>
2020-04-19 15:56:37 +0700

    4 files modified

--- src/app/admin/product-details/product-details.component.ts
+++ src/app/admin/product-details/product-details.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from "../book.service";
+import { ActivatedRoute } from "@angular/router";
+import { Book } from "./Book"
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+export class ProductDetailsComponent implements OnInit {
+book;
+  constructor(
+    private bookService: BookService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.getBook();
   }
-
+  getBook(){
+    this.activatedRoute.params.subscribe(param => {
+      // console.log(param.name);
+      // this.product = this.productService.getProduct(param.name);
+      this.bookService.getBook(param.id).subscribe(data => 
+      {
+        this.book=data
+      }
+      )
+    });
+  }
 }
